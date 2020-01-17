5526bf1b0a9e64737123f17d85d9c8ac0fcdaf26
Yuhao Xue <yx161@duke.edu>
2020-01-17 15:10:34 -0500

    update calculater

--- calculater.py
+++ calculater.py
@@ -1,34 +1,61 @@
 # calculater.py
 
-def add(x, y):
+def add(x, y): #add function
     z=x+y
-    print("{}+{}={}".format(x, y, z))
-    return z
+    symbol = "+"
+    return z, symbol #substract function
     
 def sub(x, y):
     z=x-y
-    print("{}-{}={}".format(x, y, z))
-    return z
+    symbol = "-"
+    return z, symbol
     
-def mul(x, y):
+def mul(x, y): #multiply function
     z=x*y
-    print("{}*{}={}".format(x, y, z))
-    return z
+    symbol = "*"
+    return z, symbol
+    
+def div(x, y): #divide function
+        z=x/y
+        symbol = "/"
+        return z, symbol
+    
+def inp (): #input number
+    a = input ()
+    num = float(a)
+    return num
+    
+def dis(w, x, y, z): #output answer
+    print("{}{}{}={}".format(w, x, y, z))
+    return
+    
 
-x = input ("Enter a letter:")
-print ("You entered {}".format (x))
-if x == "a":
-    d = add(55, 60)
-    if d>100:
-        print("this number is too high.")
-elif x == "s":
-    c = sub(55, 60)
-    if c<0:
-        print("this number is too low")
-elif x == "m":
-    c=mul(2, 3)
-    if c==6:
-        print("this number is amazing")
+
+
+#main
+print("<number1 symbol number2>") #enter first number
+print("Please enter your number1:")
+a=inp()
+print("Please enter your number2:") #enter second number
+b=inp()
+print("Please choose your option by number:" ) #choose option
+print("1. add   2. substract    3. multiply   4. divide")
+o=inp()
+if o == 1: #calculate
+    ans,sym = add(a, b)
+    dis(a, sym, b, ans)
+elif o == 2:
+    ans,sym = sub(a, b)
+    dis(a, sym, b, ans)
+elif o == 3:
+    ans, sym = mul(a, b)
+    dis(a, sym, b, ans)
+elif o == 4:
+    if b==0:
+        print("divisor can't be 0")
+    else:
+        ans,sym = div(a, b)
+        dis(a, sym, b, ans)
 else:
-    print("The {} command is not recognized.".format(x))
-print("done")
+    print ("you entered a wrong number")
+print("Done")
